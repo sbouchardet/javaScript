0f663f917e7ca90e8458d325e5b46ee370e33c1b
Susana Bouchardet <susana.bouchardet@gmail.com>
2016-03-18 12:36:16 -0300

    +1

--- teste.js
+++ teste.js
@@ -1,3 +1,22 @@
-function t (a,b){
-document.write(""+(a+b))
+function t(a, b) {
+document.write(a + b);
+
+}
+
+function cl() {
+    var name="Susana"
+    
+    this.getName = function(){
+        return name;
+    }
+    this.setName = function(n){
+        name=n;
+    }
+    
+}
+
+function ol() {
+     n =new cl();
+    n.setName("Suuuu")
+    document.getElementById('name').innerHTML = n.getName();
 }
